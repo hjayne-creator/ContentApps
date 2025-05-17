6f2ae52f59f2326f38b9f784d233dbc3091ed101
Houston Jayne <hjayne2@yahoo.com>
2025-05-17 16:12:58 -0700

    Remove all files from repository

--- celery_worker.py
+++
@@ -1,4 +0,0 @@
-from apps.content_plan.celery_config import celery
-
-# This file is used by Render to start the Celery worker
-# It imports and re-exports the Celery instance from your tasks module 
